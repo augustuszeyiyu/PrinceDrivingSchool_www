b7d4adc0a2e339b8792cb365d1694d9e79b428b2
Cloud Yu <j.cloud.yu@gmail.com>
2020-02-10 02:42:54 +0800

    1. Remove kernel dependencies to extes's error 2. Rewrite error handling object 3. Update version to 0.9.2

--- kernel-info.esm.js
+++ kernel-info.esm.js
@@ -6,7 +6,7 @@ import {Version} from "jsboost/version.esm.js";
 
 
 // Arch kernel - https://github.com/JCloudYu/node.prototype.kernel/
-const KernelArchVersion = [{identifier: "node.prototype.kernel", version: "0.9.1"}];
+const KernelArchVersion = [{identifier: "node.prototype.kernel", version: "0.9.2"}];
 
 // other application arch type can add additional versions like following line
 // KernelArchVersion.push({identifier: "other_arch_versions", version: "0.9.0"});
