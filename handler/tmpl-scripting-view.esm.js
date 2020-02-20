249d765f917b1714758f122e84aa16993e53a04d
Cloud Yu <j.cloud.yu@gmail.com>
2020-02-20 12:53:10 +0800

    Make the script related path information be absolute paths started from project root

--- handler/tmpl-scripting-view.esm.js
+++ handler/tmpl-scripting-view.esm.js
@@ -15,7 +15,7 @@ import {WorkingRoot} from "/kernel-info.esm.js";
 
 export async function Handle(req, res) {
 	const script_root = PurgeRelativePath(Config.server.script_root);
-	const target_url = PurgeRelativePath(decodeURIComponent(req.info.url.path||''));
+	const target_url  = PurgeRelativePath(decodeURIComponent(req.info.url.path||''));
 	
 	let matched_path = null, matched_path_dir = '', remained_path = '', candidate_base = target_url;
 	while(candidate_base !== "") {
@@ -39,8 +39,8 @@ export async function Handle(req, res) {
 				const stat = fs.statSync(test_path);
 				if ( !stat.isFile() ) continue;
 				
-				matched_path = candidate_path;
-				matched_path_dir = candidate_base;
+				matched_path = script_root + candidate_path;
+				matched_path_dir = script_root + candidate_base;
 				if ( index > 0 ) {
 					remained_path = candidates[0] + remained_path;
 				}
@@ -61,9 +61,9 @@ export async function Handle(req, res) {
 		throw new HTTPRequestRejectError(BaseError.RESOURCE_NOT_FOUND);
 	}
 	
-	req.info.url.path = remained_path;
+	req.info.url.path		 = remained_path;
 	req.info.url.script_path = matched_path;
-	req.info.url.script_dir = matched_path_dir;
+	req.info.url.script_dir	 = matched_path_dir;
 	
 	
 	const {default:handler} = await import(script_root + matched_path);
