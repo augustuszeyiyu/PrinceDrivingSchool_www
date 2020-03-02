92486dcc7ac412530e04f6191a1dbc49051d4247
Cloud Yu <j.cloud.yu@gmail.com>
2020-03-02 20:40:12 +0800

    1. Make the example tool script be a timer 2. Add more accurate flow controlling logic

--- .helper-scripts/tool-script.sample.esm.js
+++ .helper-scripts/tool-script.sample.esm.js
@@ -14,8 +14,40 @@ import {RuntimeDir} from "/kernel/runtime.esm.js";
 	// INFO: Initialize runtime here...
 	
 	
+	
+	
+	
+	
 	// INFO: Do script content here...
 	logger.info( `Tool ${import.meta.url} initialized...` );
 	logger.info( `Runtime Dir: ${RuntimeDir}` );
 	logger.info( `Working Root: ${WorkingRoot}` );
+	logger.info( `Starting regular timer...` );
+	
+	let prev = 0;
+	const timer = setInterval.create();
+	timer(()=>{
+		const now = new Date();
+		if ( (now.getTime() - prev) < 1000 ) return;
+		prev = now.getTime();
+		
+		
+		logger.info( "NOW: " + now.toLocaleISOString() );
+	}, 500);
+	
+	
+	
+	
+	process
+	.on( 'SIGNAL_INTERRUPTION', ()=>{})
+	.on( 'SIGNAL_TERMINATION', async()=>{
+		// INFO: Cleanup runtime here...
+		logger.info( `Cleaning up regular timer...` );
+		timer.clear();
+		
+		logger.info( `Exiting...` );
+		setTimeout(()=>process.exit(1));
+	});
+	
+	
 })().catch((e)=>{setTimeout(()=>{throw e;})});
