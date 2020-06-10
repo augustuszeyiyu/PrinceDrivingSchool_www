a866f5851853496e0aa512c816629f8ba02cc11a
Cloud Yu <j.cloud.yu@gmail.com>
2020-06-10 11:23:40 +0800

    Add http-stream-helper script

--- kernel-info.esm.js
+++ kernel-info.esm.js
@@ -9,7 +9,7 @@ import {Version} from "/kernel/version.esm.js";
 const KernelArchVersion = [{identifier: "node.prototype.kernel", version: "1.1.0"}];
 
 // Arch api - https://github.com/JCloudYu/node.prototype.api/
-KernelArchVersion.push({identifier: "node.prototype.api", version: "1.0.0"});
+KernelArchVersion.push({identifier: "node.prototype.api", version: "1.0.1"});
 
 // other application arch type can add additional versions like following line
 // KernelArchVersion.push({identifier: "other_arch_versions", version: "0.9.0"});
